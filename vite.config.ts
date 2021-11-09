16d90b9723177032b23633ba70d3e052212add05
ozelotjp <contact@ozelot.dev>
2021-11-09 12:05:32 +0900

    setup base path

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,8 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+
   plugins: [
     vue({
       template: { transformAssetUrls },
