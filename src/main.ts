b967f3b92e592e6aba4102af82d86f9885f552b9
ozelotjp <contact@ozelot.dev>
2021-11-23 02:02:19 +0000

    feat: language set (ja)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createPinia } from 'pinia';
 import { Quasar } from 'quasar';
+import quasarLang from 'quasar/lang/ja';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
@@ -8,7 +9,7 @@ import 'quasar/src/css/index.sass';
 
 // prettier-ignore
 createApp(App)
-  .use(Quasar)
+  .use(Quasar, {lang: quasarLang})
   .use(router)
   .use(createPinia())
   .mount('#app');
