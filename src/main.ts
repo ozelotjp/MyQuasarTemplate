a1fb7461e85b68b8548bf0b3383457ee8de981e0
ozelotjp <contact@ozelot.dev>
2021-11-26 09:51:19 +0900

    style: fix

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import { createPinia } from "pinia";
-import { Quasar } from "quasar";
-import quasarLang from "quasar/lang/ja";
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import "@quasar/extras/material-icons/material-icons.css";
-import "quasar/src/css/index.sass";
+import { createPinia } from 'pinia';
+import { Quasar } from 'quasar';
+import quasarLang from 'quasar/lang/ja';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import '@quasar/extras/material-icons/material-icons.css';
+import 'quasar/src/css/index.sass';
 
 createApp(App)
   .use(Quasar, { lang: quasarLang })
   .use(router)
   .use(createPinia())
-  .mount("#app");
+  .mount('#app');
