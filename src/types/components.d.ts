1008cf342796b9d48864bf1639bbd5ed20b93e81
ozelot <contact@ozelot.dev>
2022-04-10 16:22:53 +0900

    chore(deps): update dependencies

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -2,7 +2,7 @@
 // We suggest you to commit this file into source control
 // Read more: https://github.com/vuejs/vue-next/pull/3399
 
-declare module 'vue' {
+declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     AppAlert: typeof import('./../components/App/AppAlert.vue')['default']
     TheHeader: typeof import('./../components/App/TheHeader.vue')['default']
